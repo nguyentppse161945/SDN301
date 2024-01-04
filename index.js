09029807e3ddcb2da937644b6b33e8a3f95a18f4
immortalsobject <immortalsobject@gmail.com>
2024-01-04 17:06:58 +0700

    ex4

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const http = require('http');
-const hostname = 'localhost';
+const express = require("express"),
+  http = require("http");
+const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req,res)=>{
-    console.log(req.headers);
-    res.statusCode=200;
-    res.setHeader('Content-type','text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+const app = express();
 
-})
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+});
 
-server.listen(port,hostname,()=>{ console.log(`Server running at http://${hostname}:${port}/`);
-
+const server =http.createServer(app);
+server.listen(port,hostname,()=>{
+console.log(`Server running at http://${hostname}:${port}/`);
 })
