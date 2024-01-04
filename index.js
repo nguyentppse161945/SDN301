854018f4dc567cdd66ff8efb83f134b20085cb3c
immortalsobject <immortalsobject@gmail.com>
2024-01-04 16:43:17 +0700

    ex3

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-var rect = {
-    perimeter: (x, y) => (2*(x+y)),
-    area: (x, y) => (x*y)
-};
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
 
+const server = http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-type','text/html');
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+})
 
+server.listen(port,hostname,()=>{ console.log(`Server running at http://${hostname}:${port}/`);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-        console.log("The area of the rectangle is " + rect.area(l,b));
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
-}
-
-
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
-
-
+})
