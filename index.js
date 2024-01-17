48570316539bcd2f843e69478332329c03830e3c
immortalsobject <immortalsobject@gmail.com>
2024-01-17 10:55:25 +0700

    ex4

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const http = require('http');
-const hostname = 'localhost';
+const http = require("http");
+const hostname = "localhost";
 const port = 3000;
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const bodyParser = require("body-parser");
+const dishesRouter = require("./routes/dishRouter")
 
-const server = http.createServer((req,res)=>{
-    console.log(req.headers);
-    res.statusCode=200;
-    res.setHeader('Content-type','text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
 
-})
+//router
+// app.use((req,res, next)=>{
+//     console.log(req.headers);
+//     res.statusCode=200;
+//     res.setHeader('Content-type','text/html');
+//     res.end('<html><body><h1>This is Express server</h1></body></html>');
 
-server.listen(port,hostname,()=>{ console.log(`Server running at http://${hostname}:${port}/`);
+// })
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-})
+//
+
+
+// app.use(express.static(__dirname + "/public"));
+app.use(dishesRouter)
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
